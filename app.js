4a92e43733ee23e4cf6680a786594656d5e7cbde
QaleQ <aleflard@gmail.com>
2022-03-23 09:24:44 +0100

    add basic login functionality

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express');
 const mysql = require('mysql2/promise');
-const ejs = require('ejs');
 const app = express();
+const bcrypt = require('bcrypt');
+const session = require('express-session')
+
 app.set('view engine', 'ejs');
+
 app.use(express.static(`${__dirname}/public`));
+app.use(express.urlencoded({extended:true})); 
+app.use(session({ secret: process.env.SECRET }))
+
 app.listen(3000);
 
 main();
@@ -25,11 +31,45 @@ let [queryData, []] = await connection.query(`
     ,DATE_FORMAT(upload_datetime, "%d-%m-%Y") AS upload_date_day
     ,TIME(upload_datetime) AS upload_date_time
     FROM levels
-    ORDER BY levels.id ASC
+    ORDER BY upload_datetime ASC
     LIMIT 10;
   `)
 
   app.get('/', (req, res) => {
-    res.render('home', { queryData })
+    res.render('home', { queryData });
+  });
+
+  app.get('/login', (req, res) => {
+    res.render('login')
+  });
+
+  app.post('/login', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      let [ [ query ] , [ ] ] = await connection.query(`SELECT * FROM users WHERE username=?`, username)
+      let storedPassword = query.password;
+      let loggedIn = await bcrypt.compare(password, storedPassword);
+      if (!loggedIn) throw new Error('Something went wrong')
+      req.session.user_id = query.id;
+    } catch (err) {
+      res.render('login', { err })
+    }
+  });
+
+  app.get('/signup', (req, res) => {
+    res.render('signup')
+  });
+
+  app.post('/signup', async (req, res) => {
+    try {    
+      const { username, password, email } = req.body
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const userData = { username, password: hashedPassword, email };
+      let query = await connection.query(`INSERT INTO users SET ?`, userData)
+      req.session.user_id = query.id;
+      res.status(201).send()
+    } catch (err) {
+      res.render('signup', { err })
+    }
   })
 }
